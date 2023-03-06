a8a6c08e4eaa06ab336ab70668dd1f9c5b3c7c03
shc <sh@c>
2023-03-06 02:29:39 +0100

    Remove alias for launch'g nvim with vifm

--- .zsh/common/alias.zsh
+++ .zsh/common/alias.zsh
@@ -7,11 +7,6 @@ alias g='git'
 
 # typing |make| can get really cumbersome if done a couple hundred times a day
 alias m='make'
-
-
-# speed up calling nvim
-alias nv="nvim -c Vifm"
-
 # }}}
 
 # vim: filetype=zsh foldmethod=marker
