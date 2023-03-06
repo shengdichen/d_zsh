d81e5afa1730b3a79feec084ba0e7336fbf32739
shc <sh@c>
2023-03-06 02:20:27 +0100

    Move tmux-related to launch.zsh

--- .zsh/common/launch.zsh
+++ .zsh/common/launch.zsh
@@ -9,4 +9,17 @@ x_wl() {
     nohup "$@" 1>/dev/null 2>&1 &
 }
 
+# tmux-related {{{
+# starting the tmux server
+alias tstart='\
+    tmux \
+    -2 \
+    -f ~/.tmux.conf \
+    start-server \; source-file ~/.tmux/startup/default.tmux\
+'
+
+# shortcut for attaching a specific tmux session
+alias tatt='tmux -2 attach-session'
+# }}}
+
 # vim: filetype=zsh foldmethod=marker
