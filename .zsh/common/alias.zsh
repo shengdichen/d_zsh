8049da9d6ce7f21a8ff144ca13fd8bf6ee89fc39
shc <sh@c>
2023-03-06 02:32:05 +0100

    Remove comment for make-alias

--- .zsh/common/alias.zsh
+++ .zsh/common/alias.zsh
@@ -4,7 +4,6 @@ alias q='exit'
 # NOTE: unlike bash, NO extra setup necessary for auto-completion
 alias g='git'
 
-# typing |make| can get really cumbersome if done a couple hundred times a day
 alias m='make'
 
 # vim: filetype=zsh foldmethod=marker
