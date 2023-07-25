a9d6dd54651e17d8f2974500f189f3d35eccfe5e
shc <sh@c>
2023-07-25 15:33:26 +0200

    Reformat launch.zsh

--- .zsh/common/launch.zsh
+++ .zsh/common/launch.zsh
@@ -1,61 +1,78 @@
-x_wl() {
-    nohup "$@" 1>/dev/null 2>&1 &
+function __launch() {
+    function x_wl() {
+        nohup "$@" 1>/dev/null 2>&1 &
+    }
+    function x() {
+        WAYLAND_DISPLAY="" x_wl "$@"
+    }
 }
-x() {
-    WAYLAND_DISPLAY="" x_wl "$@"
+
+function __tmux() {
+    # starting the tmux server
+    alias tstart="\
+        tmux\
+        -2\
+        -f ~/.tmux.conf\
+        start-server \; source-file \"\${HOME}/.tmux/startup/default.tmux\"\
+    "
+
+    # shortcut for attaching a specific tmux session
+    alias tatt="tmux -2 attach-session"
 }
 
-# tmux-related {{{
-# starting the tmux server
-alias tstart="\
-    tmux\
-    -2\
-    -f ~/.tmux.conf\
-    start-server \; source-file \"\${HOME}/.tmux/startup/default.tmux\"\
-"
-
-# shortcut for attaching a specific tmux session
-alias tatt="tmux -2 attach-session"
-# }}}
-
-# append value for screen color-temp (typically 3700)
-alias x_gamma="x_wl gammastep -O"
-
-alias x_lyx="\
-    QT_PLUGIN_PATH=/usr/lib/qt/plugins\
-    x lyx\
-"
-
-x_jetbr() {
-    # env-set'g necessary when invoked under sway
-    _JAVA_AWT_WM_NONREPARENTING=1\
-    x_wl "$@"
+function __firefox() {
+    alias x_fd_wl="\
+        MOZ_ENABLE_WAYLAND=1\
+        x_wl firefox-developer-edition -P\
+    "
+    alias x_fd="\
+        MOZ_ENABLE_WAYLAND=0\
+        x firefox-developer-edition -P\
+    "
 }
-alias x_pcm="\
-    x_jetbr pycharm\
-"
-alias x_itj="\
-    x_jetbr idea\
-"
 
-# firefox {{{
-alias x_fd_wl="\
-    MOZ_ENABLE_WAYLAND=1\
-    x_wl firefox-developer-edition -P\
-"
-alias x_fd="\
-    MOZ_ENABLE_WAYLAND=0\
-    x firefox-developer-edition -P\
-"
-# }}}
+function __jetbrain() {
+    function x_jetbr() {
+        # env-set'g necessary when invoked under sway
+        _JAVA_AWT_WM_NONREPARENTING=1\
+        x_wl "$@"
+    }
 
-# launch as daemon
-alias x_fctx="x_wl fcitx5 -d"
+    alias x_pcm="\
+        x_jetbr pycharm\
+    "
+    alias x_itj="\
+        x_jetbr idea\
+    "
+}
 
-# for op'ns requiring keyring, e.g., lock & install
-alias poetry_nokeyring="\
-    PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring\
-    poetry\
-"
+function __misc() {
+    # append value for screen color-temp (typically 3700)
+    alias x_gamma="x_wl gammastep -O"
 
-# vim: filetype=zsh foldmethod=marker
+    alias x_lyx="\
+        QT_PLUGIN_PATH=/usr/lib/qt/plugins\
+        x lyx\
+    "
+
+    # launch as daemon
+    alias x_fctx="x_wl fcitx5 -d"
+
+    # for op'ns requiring keyring, e.g., lock & install
+    alias poetry_nokeyring="\
+        PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring\
+        poetry\
+    "
+}
+
+function main() {
+    __launch
+    __tmux
+    __firefox
+    __jetbrain
+    __misc
+
+    unfunction __launch __tmux __firefox __jetbrain __misc
+}
+main
+unfunction main
