19ca2ae0961657121a38100179d6afa7770770ba
shc <sh@c>
2023-07-25 15:06:00 +0200

    Add aliases for man with sample-langs

--- .zsh/common/alias.zsh
+++ .zsh/common/alias.zsh
@@ -8,6 +8,19 @@ alias g="git"
 
 alias m="make"
 
+function alias_man() {
+    alias man_fr="man -L fr_FR.UTF-8"
+    alias man_ru="man -L ru_RU.UTF-8"
+    alias man_de="man -L de_DE.UTF-8"
+    alias man_es="man -L es_ES.UTF-8"
+    alias man_pt="man -L pt_BR.UTF-8"
+    alias man_it="man -L it_IT.UTF-8"
+    alias man_sv="man -L sv_SV.UTF-8"
+    alias man_zh="man -L zh_TW.UTF-8"
+}
+alias_man
+unfunction alias_man
+
 # ag->fzf->EDITOR
 alias afe="\
     ag \
