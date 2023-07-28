b5cfc33307b7685884d3462b29c995186e752280
shc <sh@c>
2023-07-28 11:30:21 +0200

    Reformat visual.zsh

--- .zsh/conf/visual.zsh
+++ .zsh/conf/visual.zsh
@@ -1,30 +1,40 @@
-# use steady, beam-style cursor
-echo -ne "\e[6 q"
+function __cursor_config() {
+    # use steady, beam-style cursor
+    echo -ne "\e[6 q"
+}
 
-# PROMPT, aka, PS* {{{
-# common elements:
-#   %h OR %!
-#       ->  current history event number, i.e., "line number"
-#   %?
-#       ->  exit code of previous command
-#   %S/%s
-#       ->  turn on/off standout
-#   %F
-#       ->  use custom format
-#   %(?.<command_if_true>.<command_if_false>)
+function __prompt_config() {
+    # common elements:
+    #   %h OR %!
+    #       ->  current history event number, i.e., "line number"
+    #   %?
+    #       ->  exit code of previous command
+    #   %S/%s
+    #       ->  turn on/off standout
+    #   %F
+    #       ->  use custom format
+    #   %(?.<command_if_true>.<command_if_false>)
 
-# NOTE:
-# remember to always reset formatting before concluding the prompt; the command
-# input would otherwise bear the same formatting
+    # NOTE:
+    # remember to always reset formatting before concluding the prompt; the
+    # command input would otherwise bear the same formatting
 
-if [ $SHLVL -gt 3 ];
-then
-    PROMPT="%F{015}%#%F{none} "
-else
-    PROMPT="%S%F{015}%#%s%F{none} "
-fi
+    if [ $SHLVL -gt 3 ]; then
+        PROMPT="%F{015}%#%F{none} "
+    else
+        PROMPT="%S%F{015}%#%s%F{none} "
+    fi
 
-RPROMPT="%(?..%S%F{009}%?)%s%F{none}"
-# }}}
+    RPROMPT="%(?..%S%F{009}%?)%s%F{none}"
+}
 
-# vim: filetype=zsh foldmethod=marker
+function main() {
+    __cursor_config
+    __prompt_config
+
+    unfunction __cursor_config __prompt_config
+}
+main
+unfunction main
+
+# vim: filetype=zsh
