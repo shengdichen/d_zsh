0a390de2552c629dee3d8a95e2dcfaa542f859f8
shc <sh@c>
2023-07-28 11:17:29 +0200

    Reformat external.zsh

--- .zsh/conf/external.zsh
+++ .zsh/conf/external.zsh
@@ -1,14 +1,13 @@
-# fzf {{{
-source /usr/share/fzf/key-bindings.zsh
-source /usr/share/fzf/completion.zsh
+function __fzf_config() {
+    source /usr/share/fzf/key-bindings.zsh
+    source /usr/share/fzf/completion.zsh
 
-bindkey -M vicmd -r "\ec"
-bindkey -M viins -r "\ec"
-bindkey -M vicmd '^Y' fzf-cd-widget
-bindkey -M viins '^Y' fzf-cd-widget
-# }}}
+    bindkey -M vicmd -r "\ec"
+    bindkey -M viins -r "\ec"
+    bindkey -M vicmd '^Y' fzf-cd-widget
+    bindkey -M viins '^Y' fzf-cd-widget
+}
 
-# zsh-syntax-highlighting {{{
 # requires the package:
 #       zsh-syntax-highlighting
 #
@@ -16,13 +15,20 @@ bindkey -M viins '^Y' fzf-cd-widget
 #   per instruction in the package's GitHub page:
 #       ->  https://github.com/zsh-users/zsh-syntax-highlighting#faq
 #   this should be sourced near the end of .zshrc
-load_zsh_highlighter () {
+function __load_zsh_highlighter () {
     local path_zsh_highlighter=\
 /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
     [[ -e "${path_zsh_highlighter}" ]] && source "${path_zsh_highlighter}"
 }
-load_zsh_highlighter && unfunction load_zsh_highlighter
-# }}}
 
-# vim: filetype=zsh foldmethod=marker
+function main() {
+    __fzf_config
+    __load_zsh_highlighter
+
+    unfunction __fzf_config __load_zsh_highlighter
+}
+main
+unfunction main
+
+# vim: filetype=zsh
