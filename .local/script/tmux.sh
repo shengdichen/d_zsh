d3c0f07616d0e80c841e5d99e048215a7bc6af47
shc <sh@c>
2024-06-06 03:06:01 +0200

    feat(script): attach tmux with window

--- .local/script/tmux.sh
+++ .local/script/tmux.sh
@@ -10,7 +10,7 @@ __tmux_start() {
     tmux source-file "${HOME}/.config/tmux/script/launch.tmux"
 }
 
-__tmux_attach() {
+__tmux_attach_session() {
     if [ ! "${TMUX}" ]; then
         local _session
         _session="$(tmux list-sessions -F "#{session_name}" | fzf --reverse)"
@@ -20,12 +20,27 @@ __tmux_attach() {
     fi
 }
 
+__tmux_attach_window() {
+    local _session _window _choice
+    _choice="$(
+        tmux list-sessions -F "#{session_name}" | while read -r _session; do
+            tmux list-windows -t "${_session}" -F "#{window_name}" | while read -r _window; do
+                printf "%s> %s\n" "${_session}" "${_window}"
+            done
+        done |
+            fzf --reverse |
+            sed "s/^\(.*\)> \(.*\)$/=\1:=\2/"
+    )"
+    # must first become a client before switching
+    tmux attach-session ";" switch-client -t "${_choice}"
+}
+
 case "${1}" in
     "run")
         if ! __tmux_running; then
             __tmux_start
         else
-            __tmux_attach
+            __tmux_attach_window
         fi
         ;;
     *) ;;
