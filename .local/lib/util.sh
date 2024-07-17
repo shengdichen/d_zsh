dfcc6b7561eab20c5c6b2c437f39cb8784cb4f3a
shc <sh@c>
2024-07-17 11:15:17 +0200

    feat(util): print default-hint with fzf-opts

--- .local/lib/util.sh
+++ .local/lib/util.sh
@@ -17,13 +17,20 @@ __fzf() {
 }
 
 __fzf_opts() {
-    local _choice
+    local _is_first_opt="yes" _choice
     if ! _choice="$(
-        for _opt in "${@}"; do printf "%s\n" "${_opt}"; done | __fzf
+        for _opt in "${@}"; do
+            if [ "${_is_first_opt}" ]; then
+                printf "%s  // default\n" "${_opt}"
+                _is_first_opt=""
+            else
+                printf "%s\n" "${_opt}"
+            fi
+        done | __fzf
     )"; then
         _choice="${1}"
     fi
-    printf "%s" "${_choice}"
+    printf "%s" "${_choice}" | cut -d " " -f "1"
 }
 
 __is_root() {
