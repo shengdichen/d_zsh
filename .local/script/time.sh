7c4d657f10d4539d99c458df22442bf2b0c95bf2
shc <sh@c>
2024-09-04 17:04:19 +0200

    feat(time): add (formatted) print'g

--- .local/script/time.sh
+++ .local/script/time.sh
@@ -2,19 +2,58 @@
 
 . "${HOME}/.local/lib/util.sh"
 
-sync_time() {
-    local date_pattern="Date: "
-    "$(__sudo)" date -s "$(
-        curl -s --head http://google.com |
-            grep "^${date_pattern}" |
-            sed "s/${date_pattern}//g"
-    )" 1>/dev/null
-    "$(__sudo)" hwclock -w --utc
+__print() {
+    __more() {
+        date "+%Y.%b.%d" | toilet -f future
+        date "+%p, %V->%a; %Z@%z"
+        date "+%H:%M:%S" | toilet -f future --metal | lolcat
+    }
+
+    __default() {
+        date "+%Y.%b.%d-%H:%M:%S" | toilet -f future --metal | lolcat
+    }
+
+    __less() {
+        date "+%Y.%b.%d-%H:%M:%S"
+    }
+
+    case "${1}" in
+        "--more")
+            __more
+            ;;
+        "--less")
+            __less
+            ;;
+        *)
+            __default
+            ;;
+    esac
 }
 
-main() {
-    sync_time
-    unset -f sync_time
+__sync() {
+    printf "time/pre> %s\n" "$(__print --less)"
+
+    local _date_pattern="Date: "
+    "$(__sudo)" date -s "$(
+        curl -s --head "http://google.com" |
+            grep "^${_date_pattern}" |
+            sed "s/${_date_pattern}//g"
+    )" >/dev/null
+    "$(__sudo)" hwclock -w --utc
+
+    printf "\n"
+    printf "time/post> %s\n" "$(__print --less)"
 }
-main
-unset -f main
+
+case "${1}" in
+    "sync")
+        __sync
+        ;;
+    "print")
+        shift
+        __print "${@}"
+        ;;
+    *)
+        __print
+        ;;
+esac
