2372c4d6e0b04b7399ef896619f294ac79edd33e
shc <sh@c>
2024-09-10 03:45:19 +0000

    feat(time): util for set'g timezone

--- .local/script/time.sh
+++ .local/script/time.sh
@@ -32,6 +32,42 @@ __print() {
     esac
 }
 
+__timezone() {
+    local _zone_dir="/usr/share/zoneinfo" _zonefile
+
+    case "$(__fzf_opts "vaduz" "hk" "ny" "Москва" "manual")" in
+        "vaduz")
+            _zonefile="${_zone_dir}/Europe/Vaduz"
+            ;;
+        "hk")
+            _zonefile="${_zone_dir}/Asia/Hong_Kong"
+            ;;
+        "ny")
+            _zonefile="${_zone_dir}/America/New_York"
+            ;;
+        "Москва")
+            _zonefile="${_zone_dir}/Europe/Moscow"
+            ;;
+        "manual")
+            __f() {
+                (
+                    cd "${_zone_dir}" || exit 3
+
+                    local _t
+                    find -L "./" -mindepth 1 -printf "%P\n" | sort -n | while read -r _t; do
+                        if [ ! -d "${_t}" ]; then
+                            printf "%s\n" "${_t}"
+                        fi
+                    done | __fzf
+                )
+            }
+            _zonefile="${_zone_dir}/$(__f)"
+            ;;
+    esac
+
+    sudo ln -sf "${_zonefile}" "/etc/localtime"
+}
+
 __sync() {
     printf "time/pre> %s\n" "$(__print --less)"
 
@@ -58,6 +94,9 @@ __sync() {
 }
 
 case "${1}" in
+    "zone")
+        __timezone
+        ;;
     "sync")
         __sync
         ;;
